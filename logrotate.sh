474a2570bf3bf39de044441d590a08c588c3078c
tuarjun <tuar@protonmail.com>
2025-01-10 19:57:09 +0530

    Fix logrotate

--- logrotate.sh
+++ logrotate.sh
@@ -3,6 +3,7 @@
 folder="logs"
 for file in "$folder"/*; do
     if [[ -f "$file" ]]; then
-        tail -n 1000 "$file" > "$file""
+        tail -n 1000 "$file" > "$file.tmp"
+	mv "$file.tmp" "$file"
     fi
 done
